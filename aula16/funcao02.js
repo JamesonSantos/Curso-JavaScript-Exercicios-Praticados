a263dc95349d09db0894ce6584e3e221afb9a50e
Jameson Santos <jamesonbezerrasantos@gmail.com>
2021-10-19 18:16:40 -0300

    At

---
+++ aula16/funcao02.js
@@ -0,0 +1,6 @@
+function soma(n1=0, n2=0) { //Se n1 ou n2 não forem passados, recebem valor 0
+    return n1 + n2 //Retorna a soma de ambos
+}
+
+console.log(soma(2, 5)) // 2 passa para p primeiro parãmetro n1
+                        // 5 passa para o segundo parãmetro n2
